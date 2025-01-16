09bd8244eee87a257cd0e63935950f0951a63e74
bruno <bruno.fogazzi.passuello@gmail.com>
2025-01-16 11:42:03 -0300

    Instantiation data base

---
+++ src/main/java/com/brunopassu/springmongodb/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.brunopassu.springmongodb.config;
+
+import com.brunopassu.springmongodb.domain.User;
+import com.brunopassu.springmongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
