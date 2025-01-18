79424bd4cbeed0cb41c94a182659e604794276a8
bruno <bruno.fogazzi.passuello@gmail.com>
2025-01-18 11:25:01 -0300

    users posts reference

--- src/main/java/com/brunopassu/springmongodb/config/Instantiation.java
+++ src/main/java/com/brunopassu/springmongodb/config/Instantiation.java
@@ -43,5 +43,8 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, sdf.parse("17-01-2025"), "Bom dia!", "Acordei agora", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
