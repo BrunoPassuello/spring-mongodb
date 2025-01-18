4a6c90ee4a89555ecb8e49172edfb34234ce3c3f
bruno <bruno.fogazzi.passuello@gmail.com>
2025-01-18 11:48:01 -0300

    comments in posts

--- src/main/java/com/brunopassu/springmongodb/config/Instantiation.java
+++ src/main/java/com/brunopassu/springmongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.brunopassu.springmongodb.config;
 import com.brunopassu.springmongodb.domain.Post;
 import com.brunopassu.springmongodb.domain.User;
 import com.brunopassu.springmongodb.dto.AuthorDTO;
+import com.brunopassu.springmongodb.dto.CommentDTO;
 import com.brunopassu.springmongodb.repository.PostRepository;
 import com.brunopassu.springmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("16-01-2025"), "Partiu viagem", "Vou para POA!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("17-01-2025"), "Bom dia!", "Acordei agora", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("16-01-2025"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("17-01-2025"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("18-01-2025"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
